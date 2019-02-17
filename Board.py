fcf66becd69438e7adffcbdfd089fd2ae55331bb
ThePolishComrade <47540927+ThePolishComrade@users.noreply.github.com>
2019-02-17 15:48:46 -0500

    Board class to create board object
    This is a class that creates a board object using object oriented
    programming.
    
    This is done to have an object to interact with when building 
    code. A list of lists represents each space on the 8x8 board, 
    which each space having a unique index in this list of lists.

---
+++ Board.py
@@ -0,0 +1,20 @@
+FREE = 0
+WHITE = 1
+BLACK = 2
+
+
+class Board:
+
+    """ Grid for which game will be played on """
+
+    def __init__(self):
+
+        """ Creates the board """
+
+        # creates an 8x8 game board using a list of lists
+        self.grid = [[FREE]*8 for n in range(8)]
+        self.grid[3][4] = BLACK
+        self.grid[4][3] = BLACK
+        self.grid[3][3] = WHITE
+        self.grid[4][4] = WHITE
+        self.logical_moves = []
