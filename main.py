c5c5b088256bb9d64b0e46b1356b05e09240b788
aaamer0623 <47541064+aaamer0623@users.noreply.github.com>
2019-03-02 23:09:07 -0500

    Add the initialistion of Flipsies
    Method that initialises the game.
    
    The method stars the game and shows the options screen starts the game modules such as the board, etc

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-hi
+import pygame
+import ui
+import player
+import board
+ 
+ 
+BLACK = 1
+WHITE = 2
+ 
+ 
+class Main:
+    """Game main class."""
+ 
+    def __init__(self):
+        """ Show options screen and start game modules"""
+        # start
+        self.gui = ui.Gui()
+        self.board = board.Board()
+        self.get_options()
