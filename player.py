8260cda634097e55e674c9a7dc6443b5893cd25b
aaamer0623 <47541064+aaamer0623@users.noreply.github.com>
2019-03-03 14:13:20 -0500

    Add method to switch color of the disks
    This method only decides what color the disk will be based on its current color.
    
    If black then it should be switched to white, and vice versa. More functions must be used for the disk to actually flip.

--- player.py
+++ player.py
@@ -1,1 +1,10 @@
-hi
+BLACK = 1
+WHITE = 2
+
+
+def switch_color(color):
+  """Decides to change the color of the disk"""
+    if color == BLACK:
+        return WHITE
+    else:
+        return BLACK
