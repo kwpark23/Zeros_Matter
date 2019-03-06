6363e3b75042b5bcfeb0babfedbeefc222f848d6
aaamer0623 <47541064+aaamer0623@users.noreply.github.com>
2019-03-06 16:11:11 -0500

    Add the get_move method to player
    The method get_move calls a few functions to apply the move made by the player. 
    
    We identify the available positions of the given color using find_valid_moves and assign it to validMoves. We call the function get_mouse_input to get the exact position clicked by the mouse on the board and if the move is valid, we terminate the current loop and call apply_move to finally execute the move.

--- player.py
+++ player.py
@@ -16,3 +16,14 @@ def switch_color(color):
     def __init__(self, gui, color="black"):
         self.color = color
         self.gui = gui
+        
+        def get_move(self):
+        """ Uses gui to handle mouse
+        """
+        validMoves = self.current_board.get_valid_moves(self.color)
+        while True:
+            move = self.gui.get_mouse_input()
+            if move in validMoves:
+                break
+        self.current_board.apply_move(move, self.color)
+        return 0, self.current_board
