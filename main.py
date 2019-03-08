5820710c8959d3293cfda3d020fa752631e8c96f
aaamer0623 <47541064+aaamer0623@users.noreply.github.com>
2019-03-08 14:41:24 -0500

    Add a method to run game
    The method run uses an object, clock, to help track time and control the game's framerate
    
    It checks if the game is over by calling game_end then proceeds to determine the winner by counting the disks of each color as well as empties, using the function count_tiles. The winner screen is displayed by calling show_winner. pygame.time.wait(3000) pauses the game for 3000 milliseconds then the game restarts using restart_flipsies.

--- main.py
+++ main.py
@@ -29,6 +29,29 @@ class Flipsies:
 
         self.gui.show_game()
         self.gui.update(self.board.board, 2, 2, self.now_playing.color)
+        
+    def run(self):
+        clock = pygame.time.Clock()
+        while True:
+            clock.tick(60)
+            if self.board.game_ended():
+                blacks, whites, empty = self.board.count_tiles()
+                if blacks > whites:
+                    winner = BLACK
+                elif whites > blacks:
+                    winner = WHITE
+                else:
+                    winner = None
+                break
+            self.now_playing.get_current_board(self.board)
+            if self.board.find_valid_moves(self.now_playing.color) != []:
+                score, self.board = self.now_playing.get_move()
+                whites, blacks, empty = self.board.count_tiles()
+                self.gui.update(self.board.board, blacks, whites, self.now_playing.color)
+            self.now_playing, self.other_player = self.other_player, self.now_playing
+        self.gui.show_winner(winner)
+        pygame.time.wait(3000)
+        self.restart_flipsies()
 
     def restart_flipsies(self):
         self.board = board.Board()
