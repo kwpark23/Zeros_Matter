56515fb9b20709423638cbcd4e053e9f2589dc5e
Kwangjin park <hijin23@hotmail.com>
2019-03-09 14:50:51 -0500

    Debug get_options method

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import pygame
 import ui
 import player
 import board
- 
- 
+
+
 BLACK = 1
 WHITE = 2
- 
- 
-class Flipsies:
-    """Game main class."""
- 
+
+
+class Othello:
+    """
+    Game main class.
+    """
+
     def __init__(self):
         """ Show options screen and start game modules"""
         # start
@@ -29,41 +31,40 @@ class Flipsies:
 
         self.gui.show_game()
         self.gui.update(self.board.board, 2, 2, self.now_playing.color)
-        
+
     def run(self):
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
             if self.board.game_ended():
-                blacks, whites, empty = self.board.count_tiles()
-                if blacks > whites:
-                    winner = BLACK
-                elif whites > blacks:
+                whites, blacks, empty = self.board.count_stones()
+                if whites > blacks:
                     winner = WHITE
+                elif blacks > whites:
+                    winner = BLACK
                 else:
                     winner = None
                 break
             self.now_playing.get_current_board(self.board)
-            if self.board.find_valid_moves(self.now_playing.color) != []:
+            if self.board.get_valid_moves(self.now_playing.color) != []:
                 score, self.board = self.now_playing.get_move()
-                whites, blacks, empty = self.board.count_tiles()
+                whites, blacks, empty = self.board.count_stones()
                 self.gui.update(self.board.board, blacks, whites, self.now_playing.color)
             self.now_playing, self.other_player = self.other_player, self.now_playing
         self.gui.show_winner(winner)
         pygame.time.wait(3000)
-        self.restart_flipsies()
+        self.restart()
 
-    def restart_flipsies(self):
+    def restart(self):
         self.board = board.Board()
         self.get_options()
         self.run()
 
-        
+
 def main():
-    game = Flipsies()
+    game = Othello()
     game.run()
 
 
 if __name__ == '__main__':
     main()
-     
