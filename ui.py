d99fdf217ec89027b7ebeaa0952fd49fb998d7bb
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2019-03-15 13:07:54 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -18,10 +18,8 @@ class Gui:
 
         # colors
         self.BLACK = (0, 0, 0)
-        self.BACKGROUND = (0, 162, 232)
         self.WHITE = (255, 255, 255)
-        self.YELLOW = (255, 255, 0)
-        self.RED = (255, 0, 0)
+        self.TEAL = (112, 174, 199)
 
         # display
         self.SCREEN_SIZE = (640, 480)
@@ -34,8 +32,8 @@ class Gui:
         # messages
         self.BLACK_LABEL_POS = (25, self.SCREEN_SIZE[1] / 4)
         self.WHITE_LABEL_POS = (540, self.SCREEN_SIZE[1] / 4)
-        self.font = pygame.font.SysFont("Times New Roman", 50)
-        self.scoreFont = pygame.font.SysFont("Serif", 58)
+        self.font = pygame.font.SysFont("Avenir", 50)
+        self.scoreFont = pygame.font.SysFont("Avenir", 58)
 
         # image files
         self.board_img = pygame.image.load(os.path.join("images", "board.bmp")).convert()
@@ -51,15 +49,30 @@ class Gui:
         player2 = HUMAN
 
         while True:
-            self.screen.fill(self.BACKGROUND)
+            pygame.draw.rect(self.screen, self.TEAL, pygame.Rect(20, 20, 610, 430), 10)
+            pygame.draw.circle(self.screen, self.WHITE, (340, 270), 30, 0)
+            pygame.draw.circle(self.screen, self.WHITE, (300, 250), 33, 3)
+            pygame.draw.circle(self.screen, self.BLACK, (300, 250), 30, 0)
 
-            title_fnt = pygame.font.SysFont("Times New Roman", 150)
-            title = title_fnt.render("Othello", True, self.WHITE)
+            # Title
+            title_fnt = pygame.font.SysFont("Avenir", 150)
+            title = title_fnt.render("Flipsies", True, self.TEAL)
             title_pos = title.get_rect(centerx=self.screen.get_width() / 2, centery=160)
-            start_txt = self.font.render("Press Start", True, self.WHITE)
-            start_pos = start_txt.get_rect(centerx=self.screen.get_width() / 2, centery=280)
+
+            # Start Button
+            start_fnt = pygame.font.SysFont("Avenir", 80)
+            start_txt = start_fnt.render("PLAY", True, self.TEAL)
+            start_pos = start_txt.get_rect(centerx=self.screen.get_width() / 2, centery=350)
+
+            # Instructions label
+            ins_fnt = pygame.font.SysFont("Avenir", 50)
+            ins_txt = ins_fnt.render("How to play", True, (97, 148, 175))
+            ins_pos = start_txt.get_rect(centerx=self.screen.get_width() / 2.3, centery=430)
+
+            # Add all to screen
             self.screen.blit(title, title_pos)
             self.screen.blit(start_txt, start_pos)
+            self.screen.blit(ins_txt, ins_pos)
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -88,7 +101,7 @@ class Gui:
 
         # draws initial screen
         self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.background.fill(self.BACKGROUND)
+        self.background.fill(self.TEAL)
         self.score_size = 50
         self.score1 = pygame.Surface((self.score_size, self.score_size))
         self.score2 = pygame.Surface((self.score_size, self.score_size))
@@ -113,21 +126,10 @@ class Gui:
         elif color == WHITE:
             img = self.white_img
         else:
-            pass
+            img = self.tip_img
         x = pos[0] * self.SQUARE_SIZE + self.BOARD[0]
         y = pos[1] * self.SQUARE_SIZE + self.BOARD[1]
         self.screen.blit(img, (x, y), img.get_rect())
-        pygame.display.flip()
-
-    def clear_square(self, pos):
-        """ Puts in the given position a background image, to simulate that the
-        piece was removed.
-        """
-        # flip orientation
-        pos = (pos[1], pos[0])
-        x = pos[0] * self.SQUARE_SIZE + self.BOARD[0]
-        y = pos[1] * self.SQUARE_SIZE + self.BOARD[1]
-        self.screen.blit(self.clear_img, (x, y), self.clear_img.get_rect())
         pygame.display.flip()
 
     def get_mouse_input(self):
@@ -171,21 +173,17 @@ class Gui:
         pygame.display.flip()
 
     def showScore(self, blackStr, whiteStr, current_player_color):
-        black_background = self.BACKGROUND
-        white_background = self.BACKGROUND
-
-
         font = pygame.font.SysFont("ariel", 30)
-        msgw = font.render("White", True, self.WHITE)
-        msgb = font.render("Black", True, self.BLACK)
+        msgw = font.render("Player 1", True, self.WHITE)
+        msgb = font.render("Player 2", True, self.BLACK)
         self.screen.blit(msgw, (self.WHITE_LABEL_POS[0], self.WHITE_LABEL_POS[1] + 15))
         self.screen.blit(msgb, (self.BLACK_LABEL_POS[0], self.BLACK_LABEL_POS[1] + 15))
 
 
-        text = self.scoreFont.render(blackStr, True, self.BLACK, black_background)
-        text2 = self.scoreFont.render(whiteStr, True, self.WHITE, white_background)
-        self.screen.blit(text, (self.BLACK_LABEL_POS[0], self.BLACK_LABEL_POS[1] + 40))
-        self.screen.blit(text2, (self.WHITE_LABEL_POS[0], self.WHITE_LABEL_POS[1] + 40))
+        blackscore = self.scoreFont.render(blackStr, True, self.BLACK, self.TEAL)
+        whitescore = self.scoreFont.render(whiteStr, True, self.WHITE, self.TEAL)
+        self.screen.blit(blackscore, (self.BLACK_LABEL_POS[0], self.BLACK_LABEL_POS[1] + 40))
+        self.screen.blit(whitescore, (self.WHITE_LABEL_POS[0], self.WHITE_LABEL_POS[1] + 40))
 
     def wait_quit(self):
         # wait user to close window
