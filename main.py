794d4449798982ea39c3b2a8f375516e858699e4
aaamer0623 <47541064+aaamer0623@users.noreply.github.com>
2019-03-19 20:15:00 -0400

    Add method to get player options
    The function sets up player 1 and player 2 by calling 
    show_options from the UI class which returns the 
    user’s choice(s). It then assigns colors to the players
     (black for P1, white for P2) and finally displays the 
    game screen accordingly using the functions 
    show_game and update_screen.

--- main.py
+++ main.py
@@ -21,3 +21,19 @@ class Main:
         self.gui = ui.Gui()
         self.board = board.Board()
         self.get_options()
+        
+        
+    def get_options(self) -> None:
+        """
+        Sets up two players who will participate in the game.
+        """
+        # set up players
+        player1, player2 = self.gui.title_screen()
+        if player1 == "human":
+            self.now_playing = player.Human(self.gui, BLACK)
+
+        if player2 == "human":
+            self.other_player = player.Human(self.gui, WHITE)
+
+        self.gui.show_game()
+        self.gui.update_screen(self.board.board, 2, 2, self.now_playing.color)
